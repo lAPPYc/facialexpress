eb83e24babd2ed31f50464a185f94a719e0cd97a
lAPPYc <25300140+lAPPYc@users.noreply.github.com>
2017-07-23 11:52:35 +0530

    predict working. Have to correct train now

--- predict.py
+++ predict.py
@@ -2,29 +2,31 @@ from face import face_capture
 from processing import normalize
 import cv2
 from sklearn.externals import joblib
+import dlib
+
+predictor = 'lib/shape_predictor_68_face_landmarks.dat'
+predictor = dlib.shape_predictor(predictor)
+detector = cv2.CascadeClassifier("lib/haarcascade_frontalface_default.xml")
+
 
 expressions =  ['neutral','smile','fear','disgust','anger','surprise','curious']
 w = 100; h = 120; eye_dist = 20
+cam = cv2.VideoCapture(0)
 
 while(1):
+
+	img, face, positions = face_capture(cam, detector, predictor)	
+	cv2.imshow('output',img)
 	
-	print 'captureing'
-	img, face, positions = face_capture(1)
-	print 'captured, analyzing'
-	if len(face) != 1:
-		continue	
-	
+	if cv2.waitKey(1) & len(face) != 1:
+		continue
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
 	positions = normalize(face, positions, w, h, eye_dist)
-	print 'normalized'
 	clf = joblib.load('classifier.pkl')
-	print 'loaded'
 	label = clf.predict(positions)
 
 	print label
 
 	cv2.putText(img,str(label),(100,100),fontFace = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, fontScale = 4, color=(0,0,255))
-
-	cv2.imshow('output',img)
-
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
