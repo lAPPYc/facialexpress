deb80267848b44906093e3e35c8cce68369da613
lAPPYc <25300140+lAPPYc@users.noreply.github.com>
2017-08-15 16:07:32 +0530

    pictrain

--- predict.py
+++ predict.py
@@ -10,8 +10,6 @@ predictor = 'lib/shape_predictor_68_face_landmarks.dat'
 predictor = dlib.shape_predictor(predictor)
 detector = cv2.CascadeClassifier("lib/haarcascade_frontalface_default.xml")
 
-clf = joblib.load('classifier.pkl')
-
 img_path = 'training_dataset/pics/jaffe/'
 lis = [i for i in os.listdir(img_path) if 'tiff' in i]
 #lis = ['KA.AN1.39.tiff','KA.DI1.42.tiff','KA.FE3.47.tiff','KA.HA1.29.tiff','KA.NE1.26.tiff','KA.SA2.34.tiff','KA.SU3.38.tiff']
@@ -32,12 +30,15 @@ def trial():
 			[x,y,w,h] = [0]*4
 
 		positions = normalize(face, positions, w, h, eye_dist)
-		positions = np.array(positions)
-		positions = positions.reshape(1,68*2)
-		label = clf.predict(positions)
+		labels = []
+		print len(positions)
 		
-		print label
-		emotion = label[0]
+		for i in range(len(positions)):
+			clf = joblib.load('classifiers/'+str(i)+'.pkl')
+			label = clf.predict(positions[i])
+			labels.append(label[0])
+		
+		emotion=max(set(labels),key=labels.count)
 		
 		cv2.putText(img,emotion,(x+w/10,y+h+20),fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 1, color=(0,0,255),thickness = 2)	
 		cv2.imshow(img_name,img)
@@ -45,7 +46,7 @@ def trial():
 		if cv2.waitKey(0) & 0xFF == ord('q'):
 			cv2.destroyAllWindows()
 
-trial()
+#trial()
 	
 
 while(1):
--- train.py
+++ train.py
@@ -1,50 +1,31 @@
-def preprocessing():
-	global expressions
-	import numpy as np
-	import sys
-	sys.path.append('training_dataset')
-	import neutral
-	import happy
-	import afraid
-	import disgust
-	import anger
-	import surprise
-	import sad
+global expressions
+import numpy as np
+import sys
+sys.path.append('training_dataset')
+import neutral
+import happy
+import afraid
+import disgust
+import anger
+import surprise
+import sad
+from sklearn import svm
 
-	expressions =  {'neutral':neutral.NE,'smile':happy.HA,'fear':afraid.FE,'disgust':disgust.DI,'anger':anger.AN,'surprise':surprise.SU,'sad':sad.SA}
-
+expressions =  {'neutral':neutral.NE,'smile':happy.HA,'fear':afraid.FE,'disgust':disgust.DI,'anger':anger.AN,'surprise':surprise.SU,'sad':sad.SA}
 	
+for i in range(68):
 	features = []
 	labels = []
-	
-	for i in expressions:
-		for j in range(len(expressions[i])):
-			features.append(expressions[i][j])
-			labels.append(i)
-	
-	features = np.array(features)
-	labels = np.array(labels)
-	
-	nsamples, nx, ny = features.shape
-	features = features.reshape((nsamples,nx*ny))
-	
-	return features, labels
+	for j in expressions:
+		for k in expressions[j]:
+			features.append(k[i])
+			labels.append(j)
 
-features, labels = preprocessing()
+	clf = svm.SVC(kernel = 'rbf', C = 10000)
+	clf.fit(features, labels)
 
-def train(features_train, labels_train):
-
-	from sklearn.naive_bayes import GaussianNB
-	
-	clf = GaussianNB()
-	clf.fit(features_train, labels_train)
-	
-	return clf
-
-classifier = train(features, labels)
-
-from sklearn.externals import joblib
-joblib.dump(classifier,'classifier.pkl')
+	from sklearn.externals import joblib
+	joblib.dump(clf,'classifiers/'+str(i)+'.pkl')
 
 
 
