248b9024ea906b5c32dc726b5e848b7acf695a18
lAPPYc <25300140+lAPPYc@users.noreply.github.com>
2017-08-16 20:04:57 +0530

    Gonna try something new(old)

--- predict.py
+++ predict.py
@@ -12,17 +12,22 @@ detector = cv2.CascadeClassifier("lib/haarcascade_frontalface_default.xml")
 
 img_path = 'training_dataset/pics/jaffe/'
 lis = [i for i in os.listdir(img_path) if 'tiff' in i]
+
 #lis = ['KA.AN1.39.tiff','KA.DI1.42.tiff','KA.FE3.47.tiff','KA.HA1.29.tiff','KA.NE1.26.tiff','KA.SA2.34.tiff','KA.SU3.38.tiff']
 labels = []
+abbvr = {'NE':'neutral','HA':'happy','FE':'fear','DI':'disgust','AN':'anger','SU':'surprise', 'SA':'sad'}
 
 w = 100; h = 120; eye_dist = 20
 cam = cv2.VideoCapture(0)
 emotion = ''
 
 def trial():
+	L = []
+	emotions = []
 	for i in lis:
 		img_name = i
 		img = cv2.imread(img_path+i)
+		L.append(abbvr[img_name[3:5]])
 		face, positions = face_capture(cam=None, img_path=img_path, img_name=i, detector=detector, predictor=predictor)
 		try:
 			[[x,y,w,h]] = face
@@ -31,22 +36,33 @@ def trial():
 
 		positions = normalize(face, positions, w, h, eye_dist)
 		labels = []
-		print len(positions)
 		
-		for i in range(len(positions)):
-			clf = joblib.load('classifiers/'+str(i)+'.pkl')
-			label = clf.predict(positions[i])
+		for j in range(len(positions)):
+			clf = joblib.load('classifiers/'+str(j)+'.pkl')
+			label = clf.predict([list(positions[j])])
 			labels.append(label[0])
 		
-		emotion=max(set(labels),key=labels.count)
+		emotions.append(max(set(labels),key=labels.count))
 		
-		cv2.putText(img,emotion,(x+w/10,y+h+20),fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 1, color=(0,0,255),thickness = 2)	
+		'''cv2.putText(img,emotion,(x+w/10,y+h+20),fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 1, color=(0,0,255),thickness = 2)	
 		cv2.imshow(img_name,img)
 	
-		if cv2.waitKey(0) & 0xFF == ord('q'):
+		if cv2.waitKey(0) & 0xFF == ord('n'):
 			cv2.destroyAllWindows()
+		elif cv2.waitKey(0) & 0xFF == ord('q'):
+			cv2.destroyAllWindows()
+			exit(0)'''
 
-#trial()
+	NO = len(L)
+	correct = 0
+	for i in range(len(L)):
+		if L[i] == emotions[i]:
+			correct = correct + 1
+	accuracy = correct/float(NO)
+	print accuracy
+	exit(0)
+
+trial()
 	
 
 while(1):
--- train.py
+++ train.py
@@ -11,25 +11,20 @@ import surprise
 import sad
 from sklearn import svm
 
-expressions =  {'neutral':neutral.NE,'smile':happy.HA,'fear':afraid.FE,'disgust':disgust.DI,'anger':anger.AN,'surprise':surprise.SU,'sad':sad.SA}
+expressions =  {'neutral':neutral.NE,'happy':happy.HA,'fear':afraid.FE,'disgust':disgust.DI,'anger':anger.AN,'surprise':surprise.SU,'sad':sad.SA}
 	
 for i in range(68):
 	features = []
 	labels = []
+
 	for j in expressions:
+
 		for k in expressions[j]:
-			features.append(k[i])
+			features.append(list(k[i]))
 			labels.append(j)
 
-	clf = svm.SVC(kernel = 'rbf', C = 10000)
+	clf = svm.SVC(kernel = 'rbf')
 	clf.fit(features, labels)
 
 	from sklearn.externals import joblib
 	joblib.dump(clf,'classifiers/'+str(i)+'.pkl')
-
-
-
-
-
-
-
